b3761c4b65153321b504353cc38d016d20ab69d2
Tomasz Dudek <dudek.t98@gmail.com>
2020-11-20 20:03:49 +0100

    Add bootstrap table, get data to JSON, improve app look

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Data} from "../data";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Logistyka';
+  dane: Data | undefined;
+
+  popyt1 = '';
+  popyt2 = '';
+  podaz1 = '';
+  liczba11 = '';
+  liczba12 = '';
+  podaz2 = '';
+  liczba21 = '';
+  liczba22 = '';
+  podaz3 = '';
+  liczba31 = '';
+  liczba32 = '';
+
+  calculate() {
+    this.dane={
+      popyt1: this.popyt1,
+      popyt2: this.popyt2,
+      podaz1: this.podaz1,
+      liczba11: this.liczba11,
+      liczba12: this.liczba12,
+      podaz2: this.podaz2,
+      liczba21: this.liczba21,
+      liczba22: this.liczba22,
+      podaz3: this.podaz3,
+      liczba31: this.liczba31,
+      liczba32: this.liczba32
+    };
+
+    console.log(JSON.stringify(this.dane));
+  }
 }
---
+++ frontend/src/data.ts
@@ -0,0 +1,13 @@
+export interface Data {
+  popyt1: string;
+  popyt2: string;
+  podaz1: string;
+  liczba11: string;
+  liczba12: string;
+  podaz2: string;
+  liczba21: string;
+  liczba22: string;
+  podaz3: string;
+  liczba31: string;
+  liczba32: string;
+}
