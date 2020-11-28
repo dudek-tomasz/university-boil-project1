ace75ca43c986aa6820f2213c4cf112835c2775b
Tomasz Dudek <dudek.t98@gmail.com>
2020-11-28 18:53:40 +0100

    (ver alpha 1.1) Add blocking when data is incomplete

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -63,6 +63,7 @@ export class AppComponent {
 
   obliczone = false;
   done = false;
+  showWarning = false;
 
   wynik: Wynik = {
     income: 0,
@@ -78,47 +79,73 @@ export class AppComponent {
       d3_o2: 0
     }
   };
+  isComplete(){
+    //xDDDD
+    if (this.popyt1 == '') return false;
+    if (this.popyt2 == '') return false;
+    if (this.podaz1 == '') return false;
+    if (this.liczba11 == '') return false;
+    if (this.liczba12 == '') return false;
+    if (this.podaz2 == '') return false;
+    if (this.liczba21 == '') return false;
+    if (this.liczba22 == '') return false;
+    if (this.podaz3 =='') return false;
+    if (this.liczba31 == '') return false;
+    if (this.liczba32 == '') return false;
+    if (this.cenazak1 == '') return false;
+    if (this.cenazak2 == '') return false;
+    if (this.cenazak3 == '') return false;
+    if (this.cenasprz1 == '') return false;
+    if (this.cenasprz2 == '') return false;
+    return true;
+    //xDDDD
+  }
 
   calculate() {
-    this.dane = {
-      pop: {
-        o1: this.popyt1,
-        o2: this.popyt2
-      },
-      pod: {
-        d1: this.podaz1,
-        d2: this.podaz2,
-        d3: this.podaz3
-      },
-      cenazak: {
-        d1: this.cenazak1,
-        d2: this.cenazak2,
-        d3: this.cenazak3
-      },
-      cenasprz: {
-        o1: this.cenasprz1,
-        o2: this.cenasprz2
-      },
-      trans: {
-        d1_o1: this.liczba11,
-        d1_o2: this.liczba12,
-        d2_o1: this.liczba21,
-        d2_o2: this.liczba22,
-        d3_o1: this.liczba31,
-        d3_o2: this.liczba32
-      }
-    };
-    this.obliczone = true;
-    // @ts-ignore
-    this.http.get('http://localhost:6969/api/logistic?q=' + JSON.stringify(this.dane)).subscribe((data: Wynik) => {
-      this.wynik = data;
+    if (this.isComplete()) {
+      this.showWarning = false;
+      this.dane = {
+        pop: {
+          o1: this.popyt1,
+          o2: this.popyt2
+        },
+        pod: {
+          d1: this.podaz1,
+          d2: this.podaz2,
+          d3: this.podaz3
+        },
+        cenazak: {
+          d1: this.cenazak1,
+          d2: this.cenazak2,
+          d3: this.cenazak3
+        },
+        cenasprz: {
+          o1: this.cenasprz1,
+          o2: this.cenasprz2
+        },
+        trans: {
+          d1_o1: this.liczba11,
+          d1_o2: this.liczba12,
+          d2_o1: this.liczba21,
+          d2_o2: this.liczba22,
+          d3_o1: this.liczba31,
+          d3_o2: this.liczba32
+        }
+      };
+      this.obliczone = true;
+      // @ts-ignore
+      this.http.get('http://localhost:6969/api/logistic?q=' + JSON.stringify(this.dane)).subscribe((data: Wynik) => {
+        this.wynik = data;
+        setTimeout(() => {
+          this.done = true;
+        }, 1300);
+      });
+      console.log('http://localhost:6969/api/logistic?q=' + JSON.stringify(this.dane));
       setTimeout(() => {
-        this.done = true;
-      }, 1300);
-    });
-    console.log('http://localhost:6969/api/logistic?q=' + JSON.stringify(this.dane));
-    setTimeout(() => {
-      console.log('wynik: ' + JSON.stringify(this.wynik));
-    }, 1000);
+        console.log('wynik: ' + JSON.stringify(this.wynik));
+      }, 1000);
+    } else {
+      this.showWarning = true;
+    }
   }
 }
